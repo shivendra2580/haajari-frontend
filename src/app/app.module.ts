4fabc55bb1ca969539f30b2e597c549db14e9210
Ankit Gupta <ankit.gupta@krenai.com>
2023-10-12 10:44:12 +0530

    fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-<<<<<<< HEAD
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
 import { NgxPaginationModule } from 'ngx-pagination';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import {NgxDaterangepickerMd} from 'ngx-daterangepicker-material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-=======
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RouterModule, Routes } from '@angular/router';
-import {HttpClientModule} from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
-import { NgxPaginationModule } from 'ngx-pagination';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 
->>>>>>> 77e27e35544a7d1576ba0649f43a6497b8693dae
 
 @NgModule({
   declarations: [
@@ -31,16 +23,12 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     RouterModule,
     AppRoutingModule,
     HttpClientModule,
-<<<<<<< HEAD
     FormsModule,
     NgxPaginationModule,
-    NgxDaterangepickerMd.forRoot()
-=======
+    NgxDaterangepickerMd.forRoot(),
     ReactiveFormsModule,
-    // FormsModule,
     NgxPaginationModule,
     NgbModule
->>>>>>> 77e27e35544a7d1576ba0649f43a6497b8693dae
   ],
   providers: [],
   bootstrap: [AppComponent]
